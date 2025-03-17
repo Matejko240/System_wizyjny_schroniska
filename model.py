1fa57e73d4f7dbbee793b085c701b608fbc1f2c2
Matejko240 <161241380+Matejko240@users.noreply.github.com>
2025-03-17 16:54:08 +0100

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, Input
 
-from tensorflow.keras.layers import Dropout  # ✅ Dodaj dropout
+from tensorflow.keras.layers import Dropout  # Dodaj dropout
 
 def create_model(img_size, num_classes):
     """Tworzy i zwraca ulepszony model CNN do klasyfikacji zwierząt."""
@@ -10,11 +10,11 @@ def create_model(img_size, num_classes):
         Input(shape=(img_size, img_size, 3)),
         layers.Conv2D(32, (3,3), activation='relu'),
         layers.MaxPooling2D(2,2),
-        #Dropout(0.2),  # ✅ Dodano dropout
+        #Dropout(0.2),  # Dodano dropout
         
         layers.Conv2D(64, (3,3), activation='relu'),
         layers.MaxPooling2D(2,2),
-        #Dropout(0.3),  # ✅ Więcej dropout
+        #Dropout(0.3),  # Więcej dropout
         
         layers.Conv2D(128, (3,3), activation='relu'),
         layers.MaxPooling2D(2,2),
@@ -22,7 +22,7 @@ def create_model(img_size, num_classes):
         
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
-        #Dropout(0.5),  # ✅ Zapobiega przeuczeniu
+        #Dropout(0.5),  # Zapobiega przeuczeniu
         layers.Dense(num_classes, activation='softmax')
     ])
 
